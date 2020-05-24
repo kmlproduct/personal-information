96853846429456ea606aa01f22bb10e0314be84b
kmlproduct <kam.salaheddine@gmail.com>
2020-05-24 15:23:58 +0000

    repository personal information

---
+++ main.js
@@ -0,0 +1,43 @@
+document.getElementById('crt-resume').onclick = function(){
+    document.getElementById('h1').style.display = "none";
+    document.getElementById('container').style.display = "block";
+    document.getElementById('show-next1').style.background = "green";
+
+
+
+}
+function myNext(){
+    
+        document.getElementById('container2').style.display = "block";
+        document.getElementById('show-next2').style.background = "green";
+        document.getElementById('show-next1').style.background = "darkgray";
+        document.getElementById('container').style.display = "none"
+
+}
+function myNext2(){
+    
+    document.getElementById('container3').style.display = "block";
+    document.getElementById('show-next3').style.background = "green";
+    document.getElementById('show-next1').style.background = "darkgray";
+    document.getElementById('show-next2').style.background = "darkgray";
+    document.getElementById('container').style.display = "none";
+    document.getElementById('container2').style.display = "none"
+
+}
+function myNext3(){
+    
+    document.getElementById('container4').style.display = "block";
+    document.getElementById('show-next4').style.background = "green";
+    document.getElementById('show-next1').style.background = "darkgray";
+    document.getElementById('show-next2').style.background = "darkgray";
+    document.getElementById('show-next3').style.background = "darkgray";
+    document.getElementById('container').style.display = "none";
+    document.getElementById('container2').style.display = "none";
+    document.getElementById('container3').style.display = "none";
+
+}
+
+
+
+
+
